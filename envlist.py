f613a77aa7797560268183bc1142f691b2018141
sooclement <sooclement@live.com>
2020-10-25 02:27:57 +0800

    Update envlist.py

--- envlist.py
+++ envlist.py
@@ -43,8 +43,9 @@ with lock:
         # create a list (named clist) of nevns environments with the 
         # prefix envprefix
         # add code here
-        clist = list(range(int(nenvs)))
-        clist = [envprefix + str(x) for x in clist]
+        clist = []
+        for i in range(int(nenvs)):
+           clist.append(envprefix + str(i))
         
     else:
         # load hickle file
