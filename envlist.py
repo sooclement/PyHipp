42075e602edeca8ce35c4f3a4a9ffac59beb92e0
sooclement <sooclement@live.com>
2020-10-25 00:12:19 +0800

    Lab 7 Step 40 (envlist.py only)

---
+++ envlist.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 03:06:59 2020
+
+@author: soocl
+"""
+
+#!/data/miniconda3/envs/env1/bin/python
+
+from filelock import FileLock
+import hickle
+import sys
+
+# set constants
+file_path = "/data/picasso/envlist.hkl"
+lock_path = "/data/picasso/envlist.hkl.lock"
+time_out_secs = 60
+
+# program modes
+READ_MODE = 0
+WRITE_MODE = 1
+RESET_MODE = 2
+
+# get number of arguments
+nargs = len(sys.argv)
+if nargs > 3:
+    print('Usage: envlist; envlist env; envlist envprefix nenvs')
+    exit
+elif nargs > 2:
+    # creates or re-creates the list of environments
+    pmode = RESET_MODE
+    # get prefix for environment name
+    envprefix = sys.argv[1]
+    # get number of environments to create
+    nenvs = sys.argv[2]
+elif nargs > 1:
+    # returns env to the list of environments
+    pmode = WRITE_MODE
+    # get name of environment to return
+    env = sys.argv[1]
+else:
+    # gets name of an environment to use
+    pmode = READ_MODE
+
+# creates a lock for the file so it can only be accessed one at a time
+lock = FileLock(lock_path, timeout=time_out_secs)
+
+with lock:
+    if pmode == RESET_MODE:
+        # create a list (named clist) of nevns environments with the 
+        # prefix envprefix
+        # add code here
+        clist = list(range(64))
+        clist = ['cenv' + x for x in clist]
+        
+    else:
+        # load hickle file
+        clist = hickle.load(file_path)
+
+        if pmode == WRITE_MODE:
+            # append env to the clist
+            # add code here
+            clist.append(envprefix + nenvs)
+        else:
+            # get and remove env from clist
+            # add code here
+            # return env name
+            clist.remove(envprefix + nenvs)
+            print(env)
+            
+# save hickle file
+hickle.dump(clist, file_path, mode="w")
